672515bb8311a5d7a0606bdbbc45e2da82782adc
RomStark <rgtarkovskiy@edu.hse.ru>
2024-03-20 12:08:27 +0300

    first commit

---
+++ floraFinder/floraFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  floraFinder
+//
+//  Created by Al Stark on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
